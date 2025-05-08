43bb64100b1d668bf5904055433299390846828d
zahrafeisal <zahrafeisal3@gmail.com>
2025-05-08 23:17:18 +0300

    import

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,6 +1,6 @@
 import '../App.css';
 import React from "react";
-import { Link } from "react-router";
+import { Link } from "react-router-dom";
 import LoginForm from './LoginForm';
 import LandingNavbar from './LandingNavbar';
 
